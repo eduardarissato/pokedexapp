ef49e8129cb3e027e278162d07450c4f96a417c2
eduardarissato <eduardarissato@gmail.com>
2022-08-04 15:03:04 -0300

    3 files modified

--- src/app/services/pokemon.service.ts
+++ src/app/services/pokemon.service.ts
@@ -14,4 +14,7 @@ export class PokemonService {
   buscarTodosPokemon() {
     return this.http.get(`${this.url}pokemon`);
   }
+  buscarUmPokemon(url: string) {
+    return this.http.get(url);
+  }
 }
